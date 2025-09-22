a183a15a3a315460f0bc25afbee5e41b17329c39
jin <yusengha@naver.com>
2025-09-22 17:17:12 +0900

    [Bronze V] Title: 새싹, Time: 88 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        solve();
+    }
+    public static void solve() {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}

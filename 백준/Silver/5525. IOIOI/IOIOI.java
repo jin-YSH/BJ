03cf7274c0af111be88b1a057f4df6d79ec0ee28
jin <yusengha@naver.com>
2025-11-14 17:28:14 +0900

    [Silver I] Title: IOIOI, Time: 272 ms, Memory: 23372 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        solve();
+        solve2();
     }
     public static void solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,10 +19,7 @@ public class Main {
         String Pn=sb.toString();
 
         int count=0;
-        int length=Pn.length();
-//        System.out.println(S);
-//        System.out.println(Pn);
-//        System.out.println("start");
+
         while(S.contains(Pn)){
             int index=S.indexOf(Pn);
             S=S.substring(index+2);
@@ -30,7 +27,35 @@ public class Main {
             count++;
         }
         System.out.println(count);
+    }
+    public static void solve2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<N;i++){
+            sb.append("IO");
+        }
+        sb.append("I");
+        String Pn=sb.toString();
 
+        int length=Pn.length();
+        int count=0;
+        int answer=0;
+        for(int j=0;j<M-2;j++){
+            if(S.charAt(j)=='I'&&S.charAt(j+1)=='O'&&S.charAt(j+2)=='I'){
+                count++;
+                if(count>=N){
+                    answer++;
+                }
+                j++;
+            }
+            else {
+                count=0;
+            }
+        }
 
+        System.out.println(answer);
     }
 }

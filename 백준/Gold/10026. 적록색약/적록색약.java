e1309f654f324a789992c31d71d6daa6fcef5570
jin <yusengha@naver.com>
2025-11-19 18:00:57 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 16896 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -5,73 +5,71 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
+    static int N;
+    static char[][] normalMap;   // 정상 시각
+    static char[][] weakMap;     // 색약 시각(R/G 통합)
+    static boolean[][] visitedNormal;
+    static boolean[][] visitedWeak;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
     public static void main(String[] args) throws IOException {
         solve();
     }
     public static void solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String[][] Map1 =new String[N][N];
-        String[][] Map2 =new String[N][N];
-        boolean[][] visited1 =new boolean[N][N];
-        boolean[][] visited2 =new boolean[N][N];
-        int[] dx={0,0,1,-1};
-        int[] dy={1,-1,0,0};
+        N = Integer.parseInt(br.readLine());
+
+        normalMap = new char[N][N];
+        weakMap = new char[N][N];
+        visitedNormal = new boolean[N][N];
+        visitedWeak = new boolean[N][N];
+
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
             for (int j = 0; j < N; j++) {
-                Map1[i][j] = line.charAt(j)+"";
-                Map2[i][j] = line.charAt(j)+"";
-                if(Map2[i][j].equals("G")){
-                    Map2[i][j]="R";
-                }
+                char input = line.charAt(j);
+                normalMap[i][j] = input;
+                weakMap[i][j] = (input=='G')?'R':input;
             }
         }
-        Queue<int[]> RG=new LinkedList<>();
-        Queue<int[]> RGB=new LinkedList<>();
-        RG.offer(new int[]{0,0});
-        RGB.offer(new int[]{0,0});
-        int sizeRG=0;
-        int sizeRGB=0;
+
+        int normalCount = 0;
+        int weakCount = 0;
+
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
-                if(!visited1[i][j]){
-                    visited1[i][j]=true;
-                    RG.offer(new int[]{j,i});
-                    sizeRG++;
-                    while (!RG.isEmpty()) {
-                        int[] now=RG.poll();
-                        String nowColor= Map1[now[1]][now[0]];
-                        for(int d=0;d<4;d++) {
-                            int nx=now[0]+dx[d];
-                            int ny=now[1]+dy[d];
-                            if(nx>=0 && ny>=0 && nx<N && ny<N&& Map1[ny][nx].equals(nowColor)&& !visited1[ny][nx]) {
-                                visited1[ny][nx]=true;
-                                RG.offer(new int[]{nx,ny});
-                            }
-                        }
-                    }
+                if(!visitedNormal[i][j]){
+                    normalCount++;
+                    BFS(normalMap,visitedNormal,j,i);
                 }
-                if(!visited2[i][j]){
-                    visited2[i][j]=true;
-                    RGB.offer(new int[]{j,i});
-                    sizeRGB++;
-                    while (!RGB.isEmpty()) {
-                        int[] now=RGB.poll();
-                        String nowColor= Map2[now[1]][now[0]];
-                        for(int d=0;d<4;d++) {
-                            int nx=now[0]+dx[d];
-                            int ny=now[1]+dy[d];
-                            if(nx>=0 && ny>=0 && nx<N && ny<N&&Map2[ny][nx].equals(nowColor)&& !visited2[ny][nx]) {
-                                visited2[ny][nx]=true;
-                                RGB.offer(new int[]{nx,ny});
-                            }
-                        }
-                    }
+                if(!visitedWeak[i][j]){
+                    weakCount++;
+                    BFS(weakMap,visitedWeak,j,i);
                 }
             }
         }
-        System.out.println(sizeRG+" "+sizeRGB);
+        System.out.println(normalCount+" "+weakCount);
 
     }
+    public static void BFS(char[][] Map, boolean[][] visited, int x, int y){
+        Queue<int[]> queue = new LinkedList<>();
+        visited[y][x]=true;
+        queue.offer(new int[]{x,y});
+        char nowColor= Map[y][x];
+
+        while (!queue.isEmpty()) {
+            int[] now=queue.poll();
+
+            for(int d=0;d<4;d++) {
+                int nx=now[0]+dx[d];
+                int ny=now[1]+dy[d];
+                if(nx>=0 && ny>=0 && nx<N && ny<N&&Map[ny][nx]==nowColor&& !visited[ny][nx]) {
+                    visited[ny][nx]=true;
+                    queue.offer(new int[]{nx,ny});
+                }
+            }
+        }
+    }
+
 }

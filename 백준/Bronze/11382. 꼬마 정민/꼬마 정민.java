bd31a78d0259e5c47b68b0236f64a51e5cf4ab4c
jin <yusengha@naver.com>
2025-12-28 21:16:33 +0900

    [Bronze V] Title: 꼬마 정민, Time: 96 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String[] input=br.readLine().split(" ");
+        Long A=Long.parseLong(input[0]);
+        Long B=Long.parseLong(input[1]);
+        Long C=Long.parseLong(input[2]);
+        System.out.println(A+B+C);
+    }
+}

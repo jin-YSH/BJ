358e5485a9fa6e324684a28d768e096379db745c
jin <yusengha@naver.com>
2026-01-22 13:09:20 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 64 ms, Memory: 11888 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,39 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main(String[] args) throws Exception{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		StringBuilder sb=new StringBuilder();
+        
+		for(int y=n-1;y>=0;y--) {
+			for(int x=0;x<n-y;x++) {
+				sb.append("*");
+			}
+			for(int x=0;x<y*2;x++) {
+				sb.append(" ");
+			}
+            for(int x=0;x<n-y;x++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		for(int y=0;y<n-1;y++) {
+			for(int x=0;x<n-y-1;x++) {
+				sb.append("*");
+			}
+			for(int x=0;x<y*2+2;x++) {
+				sb.append(" ");
+			}
+            for(int x=0;x<n-y-1;x++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
+

76774e9e3d4d394146312634372c98175d430f22
jin <yusengha@naver.com>
2026-01-29 13:52:19 +0900

    [Gold II] Title: 빵집, Time: 1504 ms, Memory: 502352 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,72 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int R;
+	static int C;
+	static String[][] map;
+	static boolean[][] visited;
+	static int answer=0;
+	static boolean debug=false;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String[] input=br.readLine().split(" ");
+		R=Integer.parseInt(input[0]);
+		C=Integer.parseInt(input[1]);
+		map=new String[R][C];
+		visited=new boolean[R][C];
+		for(int r=0;r<R;r++) {
+			input=br.readLine().split("");
+			for(int c=0;c<C;c++) {
+				map[r][c]=input[c];
+			}
+		}
+		
+		if(debug==true)
+			printMap();
+		visited[0][0]=true;
+		for(int i=0;i<R;i++) {
+			if(dfs(i,0)) {
+				answer++;
+			}
+		}
+		//dfs(0,0,0,0);
+		System.out.println(answer);
+	}
+	public static void printMap() {
+		for(int r=0;r<R;r++) {
+			for(int c=0;c<C;c++) {
+				System.out.print(map[r][c]);
+			}
+			System.out.println();
+		}
+	}
+	public static boolean dfs(int r,int c) {
+		if(c==C-1) {
+			return true;
+		}
+		
+//		if(c==C-1&&start==R-1) {
+//			Math.max(count, answer);
+//			return;
+//		}
+		for(int i=-1;i<=1;i++) {
+			int nc=c+1;
+			int nr=r+i;
+			if(nc>=0&&nc<C&&nr>=0&&nr<R) {
+				if(!visited[nr][nc]&&map[r][c].equals(".")) {
+					visited[nr][nc]=true;
+					if(dfs(nr,nc)==true) {
+						return true;
+					}
+					
+					//visited[nr][nc]=false;
+				}
+			}
+		}
+		return false;
+		
+	}
+}
